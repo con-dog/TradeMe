6674ea3d97f6a48e5a75bea5ef75fde4cdae97f0
Papillon <75226598+con-dog@users.noreply.github.com>
2024-12-11 09:08:18 +1300

    Sets single default endpoint, returns response

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  if (req.query.name) {
+    res.send(`Hello, ${req.query.name}!`);
+  } else {
+    res.send('Hello World!');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
